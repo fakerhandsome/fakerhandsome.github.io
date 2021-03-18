02c18d7777861019e81274a1c57c83a2e615d2eb
faker <faker@qq.com>
2021-03-18 20:39:31 +0800

    提交一次

---
+++ docusaurus.config.js
@@ -0,0 +1,108 @@
+module.exports = {
+  title: "Power's Wiki",
+  //titleDelimiter: "🦖", // Defaults to `|`
+  tagline: "啥都玩的斜杠青年",
+  url: "https://fakerhandsome.github.io",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  projectName: 'fakerhandsome.github.io',
+  organizationName: 'fakerhandsome',
+  favicon: "https://cos.ap-guangzhou.myqcloud.com/wiki-media-1253965369/doc/logo-zip.png",
+  //organizationName: "linyuxuanlin", // Usually your GitHub org/user name.
+  //projectName: "Wiki_Docusaurus", // Usually your repo name.
+  themeConfig: {
+    sidebarCollapsible: true, //默认折叠
+    image: 'https://cos.ap-guangzhou.myqcloud.com/wiki-media-1253965369/doc/logo-zip.png',
+    algolia: {
+      apiKey: "5c07d8bf9c9928c4453857f6cad0420e",
+      indexName: "wiki-power",
+
+      // Optional: see doc section bellow
+      contextualSearch: true,
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      //... other Algolia params
+      prism: {
+        additionalLanguages: ['java'],
+      },
+    },
+    
+    colorMode: {
+      // "light" | "dark"
+      //defaultMode: "dark",
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+
+      // Dark/light switch icon options
+      switchConfig: {
+        // Icon for the switch while in dark mode
+        darkIcon: '🌙',
+        lightIcon: '🌞',
+
+        // CSS to apply to dark icon,
+        // React inline style object
+        // see https://reactjs.org/docs/dom-elements.html#style
+        darkIconStyle: {
+          marginLeft: "2px",
+        },
+
+        // Unicode icons such as '\u2600' will work
+        // Unicode with 5 chars require brackets: '\u{1F602}'
+        //lightIcon: '\u{1F602}',
+
+        lightIconStyle: {
+          marginLeft: "1px",
+        },
+      },
+    },
+
+    hideableSidebar: false,
+    navbar: {
+      title: "Power's Wiki",
+      hideOnScroll: false,
+      //style: 'primary',
+      /*
+      logo: {
+        alt: "My Site Logo",
+        src:
+          "https://wiki-media-1253965369.cos.ap-guangzhou.myqcloud.com/img/20201122195819.png",
+      },
+      */
+      items: [
+        {
+          href: "https://wiki.wildwolf.tech/",
+          label: "队内知识库",
+          position: "right",
+        },
+        {
+          href: "http://digest.wiki-power.com/",
+          label: "书摘",
+          position: "right",
+        },
+        {
+          href: "https://nav.wiki-power.com/",
+          label: "友链 & 导航站",
+          position: "right",
+        },
+      ],
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
